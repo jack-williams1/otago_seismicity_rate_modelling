9259727a74fe50c203de8e3fec938019860190dc
jack-williams1 <32389253+jack-williams1@users.noreply.github.com>
2025-04-16 14:04:26 +1200

    Update get_rupture_traces.py
    Updated to call individual logic tree branch solutions

--- nshm_inversion/solvis/scripts/get_rupture_traces.py
+++ nshm_inversion/solvis/scripts/get_rupture_traces.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 26 14:34:54 2024
-
-@author: wilja48p
-"""
-
 ##### SELECT 2022 NSHM RUPTURE SETS THAT INTERSECT AND WITHIN SEARCH POLYGON ######
 ### THEN RETURN A GEOJSON FILE FOR RUPTURES THAT INTERSECT THE POLYGON  #######
 
@@ -13,21 +5,22 @@ Created on Fri Jan 26 14:34:54 2024
 #polygon, this should be equal to the full partipation rate of that section. For rupture sections
 #outside the polygon, it is the rate for ruptures that intersect the polygon only
 
-import nzshm_model as nm
+#Can be run for inversion solution rates from individual logic tree branches (opt=1) or for 
+#combined geologic (opt=2) or geodetic weighted average (opt =3) rates. For opt=2 or 3, then
+#need to run combine_branches.py first to get weighed average rates. 
+
 import json
 import pathlib
-import geopandas 
+import geopandas
+import pandas as pd
 
 from shapely.geometry import shape
 from shapely.geometry.polygon import Polygon 
 
-from shapely.ops import transform #added by JW
-import pyproj #added by JW
 import numpy as np #added by JW
-import os #added by JW
+import os
 
-
-from solvis import CompositeSolution, FaultSystemSolution
+from solvis import InversionSolution, CompositeSolution, FaultSystemSolution
 from solvis.filter import FilterRuptureIds
 
 def export_geojson(gdf, filename, **kwargs):
@@ -35,21 +28,63 @@ def export_geojson(gdf, filename, **kwargs):
     f = open(filename, 'w')
     f.write(gdf.to_json(**kwargs))
     f.close()
+ 
 
-#Set logic tree exploration option
-# 1 = weighted mean rates from all branches
-# 2 = select certain branch
+logic_tree_branches = {
+    "geol_b0.823": "SW52ZXJzaW9uU29sdXRpb246MTEzMDUz",
+    "geol_b0.959": "SW52ZXJzaW9uU29sdXRpb246MTEzMDMy",
+    "geol_b1.089": "SW52ZXJzaW9uU29sdXRpb246MTEzMDM5",
+    "geod_b0.823": "SW52ZXJzaW9uU29sdXRpb246MTEzMDY3",
+    "geod_b0.959": "SW52ZXJzaW9uU29sdXRpb246MTEzMDgw",
+    "geod_b1.089": "SW52ZXJzaW9uU29sdXRpb246MTEzMDYz",
+}
 
-logic_tree_opt=2
 
-#select branch. 
-if logic_tree_opt==2:
-    #uncomment to print branch options
-    #ref_table=open("nshm_logictreebranch_lookuptable.txt",'r')
-    #branch_ref_table=ref_table.read()
-    #print(branch_ref_table) 
-    branch="U2NhbGVkSW52ZXJzaW9uU29sdXRpb246MTIwNzI4"
 
+# ================ MODIFY THESE LINES ================ 
+
+#Select if want rates from individual or combined branches
+#Combined branches requires running combine_branches.py first
+#1 = individual logic tree branch rates
+#2 = combined weighted mean geologic rates
+#3 = combined weighted mean geodetic rates
+
+opt=1
+
+#if opt = 1 select branch
+branch = "geol_b0.823"
+
+# set your path
+work_dir = pathlib.Path("WORK/")
+output_dir= pathlib.Path("OtagoFaults/")
+
+polygon_filename = "orb_area_polygon.geojson"
+    
+# ================ MODIFY THESE LINES ================ 
+
+#Load model
+if opt==1:
+    
+    soln_id = logic_tree_branches[branch]
+    # load individual logic tree inversion solution
+    soln_filepath = work_dir / f"{soln_id}.zip"
+    soln = InversionSolution.from_archive(soln_filepath)
+    filepath=output_dir / f"{soln_id}.geojson"
+    
+else:
+    #Need to use full logic tree solution
+    import nzshm_model as nm
+    slt = nm.get_model_version("NSHM_v1.0.4").source_logic_tree
+    comp = CompositeSolution.from_archive("WORK/NSHM_v1.0.4_CompositeSolution.zip", slt)
+
+    # get the crustal fault solutions
+    soln: FaultSystemSolution = comp._solutions['CRU'] 
+    if opt==2: #import files created in combine_patches.py
+        rates_w=pd.read_csv('OtagoFaults/geo_rates_w.csv')
+        filepath=output_dir / "geo_rates.geojson"
+    elif opt==3:
+        rates_w=pd.read_csv('OtagoFaults/ged_rates_w.csv') 
+        filepath=output_dir / "ged_rates.geojson"
 
 # load the search polygon to select ruptures
 
@@ -58,78 +93,70 @@ os.chdir("..") #Go up one level in directory
 geojson = json.load(open('gis_files/orb_area_polygon.geojson'))
 polygon: Polygon = shape(geojson['features'][0]['geometry'])
 
-# load the model
-os.chdir('nshm_inversion')
-slt = nm.get_model_version("NSHM_v1.0.4").source_logic_tree
-comp = CompositeSolution.from_archive("NSHM_v1.0.4_CompositeSolution.zip", slt)
+os.chdir("nshm_inversion/solvis") 
 
-#Go back to solvis directory
-os.chdir('solvis')
 
-# get the crustal fault solutions
-fss: FaultSystemSolution = comp._solutions['CRU']  # NB this API call will change in the future
-
-# get the rupture_ids that intersect or are within the selected polygon
-rupture_ids = FilterRuptureIds(fss).for_polygon(polygon)
+# get the rupture_ids that intersect and within the selected polygon
+if opt==1:
+    rupture_ids = FilterRuptureIds(soln).for_polygon(polygon)
+else:
+    rupture_ids = rates_w.rup_id
+    
 rupture_ids_list=rupture_ids.tolist()
 
+# extract selected ruptures from solution model
+rr = soln.model.fault_sections_with_rupture_rates
+rup_info: list = rr[rr['Rupture Index'].isin(rupture_ids)]
+rup_info_list=rup_info['Rupture Index'].tolist()
 
-#get rupture rates that are within search polygon based on logic tree opt
-
-if logic_tree_opt==1: #Use weighted rupture rates from all logic tree branches
-    rr = fss.model.ruptures_with_rupture_rates
-    rup_info: list = rr[rr['Rupture Index'].isin(rupture_ids)]
-    rup_info_list=rup_info['Rupture Index'].tolist()
-
-elif logic_tree_opt==2: #Use rupture rates from specific logic tree branch
-    cr = fss.solution_file.composite_rates 
-    #select rupture rates by logic tree branch
-    rup_info=cr[(cr["Rupture Index"].isin(rupture_ids)) & (cr.solution_id==branch)]
-    rup_info_list=rup_info['Rupture Index'].tolist()  
-    #select only rupture ids that are in logic tree branch solution
-    rupture_ids = rupture_ids.intersection(set(rup_info_list))
-    rupture_ids_list= list(rupture_ids)
-
-
-#get fault sections of ruptures
-#from line 285 at: https://github.com/GNS-Science/solvis-graphql-api/blob/181a42ee119f664a7fba16ed55c067073512935a/solvis_graphql_api/composite_solution/cached.py#L246
-fsr = fss.model.fault_sections_with_rupture_rates
 
 #get fault sections of intersecting ruptures
-rup_fs = fsr[fsr['Rupture Index'].isin(rupture_ids)]
+rup_fs = rr[rr['Rupture Index'].isin(rupture_ids)]
 rup_fs_list=rup_fs['Rupture Index'].tolist()
-rup_sec=rup_fs['section'].tolist() #all unique fault sections in rup_fs
+rup_sec=rup_fs['section'].tolist() 
 unique_rup_sec=np.unique(rup_fs['section']) #all unique fault sections in rup_fs
 
-tmp_indx1=[0]*len(unique_rup_sec)
-sec_rate=[0]*len(unique_rup_sec) 
+#If opt is 2 or 3, add combined weighted rates and save to csv file for ALL sections
+if opt!=1:
+    tmp_w_rates=np.zeros(len(rup_fs))
     
+    for kk in range(len(rupture_ids)):
+        tmp_indx1=np.where(np.isin(rup_fs['Rupture Index'],rates_w.rup_id[kk],))[0].tolist()
+        tmp_w_rates[tmp_indx1]=rates_w.w_rate[kk]
+    
+    #Add weighted average rates to rup_fs and export to csv
+    rup_fs["w_rates"] = tmp_w_rates
+
+    
+    if opt==2:
+        rup_fs.to_csv("OtagoFaults/fault_sections_geo_w.csv") 
+    elif opt==3:
+        rup_fs.to_csv("OtagoFaults/fault_sections_ged_w.csv") 
+
+tmp_indx2=[0]*len(unique_rup_sec)
+sec_rate=[0]*len(unique_rup_sec) 
+
 #loop through each unique section
 for rid in range(unique_rup_sec.size):
-    
-    tmp_indx1[rid]=rup_sec.index(unique_rup_sec[rid])
-    
-    #for each section, find all the ruptures its associated with in rup_fs
-    tmp_indx2 = np.where(np.isin(rup_fs['section'], unique_rup_sec[rid]))[0].tolist()
-    rate=[0] * len(tmp_indx2) 
-    
-    for jj in range(len(tmp_indx2)):
-        
-        #for each section's rupture, index the weighted mean rupture rate
-        if logic_tree_opt==1:
-            rate[jj]=rup_fs['rate_weighted_mean'][tmp_indx2[jj]]
-        
-        #for each section's rupture, index the rupture in rup_info and then extract rate
-        elif logic_tree_opt==2:        
-            tmp_indx3=rup_info_list.index(rup_fs_list[tmp_indx1[jj]])
-            rate[jj]=rup_info['Annual Rate'][tmp_indx3]
-     
-        #combine the section's rate for all ruptures it particpated in    
-        sec_rate[rid]=sum(rate) 
- 
 
+    tmp_indx2[rid]=rup_sec.index(unique_rup_sec[rid])
+
+    #for each section, find all the ruptures its associated with in rup_fs
+    tmp_indx3 = np.where(np.isin(rup_fs['section'], unique_rup_sec[rid]))[0].tolist()
+    rate=[0] * len(tmp_indx3) 
+
+    for jj in range(len(tmp_indx3)):
+        if opt==1:
+            rate[jj]=rup_fs.iloc[tmp_indx3[jj],2]#Use rates from branch's inversion solution
+        else:
+            rate[jj]=rup_fs.iloc[tmp_indx3[jj],26]#Use weighted average rates calculated from combined_branches.py 
+    
+    #combine the section's rate for all ruptures it particpated in    
+    sec_rate[rid]=sum(rate) 
+
+   
 #remove rows in rup_fs with duplicate sections
-rup_fs_unique=rup_fs.iloc[np.array(tmp_indx1)]
+rup_fs_unique=rup_fs.iloc[np.array(tmp_indx2)]
 
 #add section prticipation rates to dataframe
 rup_fs_unique.insert(len(rup_fs_unique.columns),"Participation_Rate",sec_rate)
@@ -137,12 +164,14 @@ rup_fs_unique.insert(len(rup_fs_unique.columns),"Participation_Rate",sec_rate)
 #convert section particpation rates to geojson file
 gdf = geopandas.GeoDataFrame(rup_fs_unique, crs="EPSG:4326")
 
-if logic_tree_opt ==1:
-    export_geojson(gdf,"WORK/otago_sections_weighted.geojson") 
-elif logic_tree_opt ==2:
-    filename_list=["WORK/otago_sections_" + branch + ".geojson"]
-    tmp_str=""
-    filename=tmp_str.join(filename_list)
-    export_geojson(gdf,filename)
+export_geojson(gdf,filepath) 
 
+#Uncomment to write rupture surfaces to geojson files (only for geologic weighted mean)
+#This is needed to run get_fault_rupture_patches.py
+
+if opt==2:
+    for rid in range(len(rupture_ids_list)):
+        rupture_surface_gdf=soln.rupture_surface(rupture_ids_list[rid])
+        rupture_surface_gdf=rupture_surface_gdf.assign(weighted_geologic_rate=rates_w.w_rate[rid]) 
+        export_geojson(rupture_surface_gdf, filename=pathlib.Path("OtagoFaults/rupture_surfaces/rupture_surface_" + str(rupture_ids[rid]) + ".geojson"))
 
